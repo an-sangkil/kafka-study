970021cd375b26ea55dc99381f727f51ccc37b02
yohan.an <an.sangkil@gmail.com>
2025-12-10 20:18:00 +0900

    add: 스트림 내부토픽 changelog 및 rocksdb 설정 추가
    기본설정이 아닌 세부설정을 추가함
    - 케싱 처리
    - changelog 토픽(스트림즈내부토픽)에 대한 세그먼트 및 리텐션 설정
    - 기본 ktable key-value-store 사용 설정

--- kafka-streams-study/src/main/java/org/example/kafkastreamstudy/topolgy/WordCountTopology.java
+++ kafka-streams-study/src/main/java/org/example/kafkastreamstudy/topolgy/WordCountTopology.java
@@ -1,14 +1,21 @@
 package org.example.kafkastreamstudy.topolgy;
 
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.WindowStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Description
@@ -23,13 +30,23 @@ public class WordCountTopology {
     @Bean
     public KStream<String, Long> kWordCountStream(StreamsBuilder streamsBuilder) {
 
+
+        // 자동 생성되는 토픽에 대한 설정 , Kafka Streams 내부 토픽 (changelog, repartition topic)
+        Map<String, String> logConfig = new HashMap<>();
+        logConfig.put("retention.ms", String.valueOf(7L * 24 * 60 * 60 * 1000)); // 7일 보관
+        logConfig.put("segment.bytes", "536870912"); // 512MB 세그먼트
+
         KStream<String, String> stream = streamsBuilder.stream("input-wordcount-topic");
         KTable<String, Long> wordCounts = stream.flatMapValues(value -> Arrays.asList(value.toUpperCase().split(" ")))
                 .groupBy((key, value) -> value)
+                //.windowedBy()
                 .count(
 
-                        Materialized
-                                .as("wordcount-store")
+                        Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("wordcount-store")
+
+                                .withCachingEnabled()               // 케싱
+                                .withLoggingEnabled(logConfig)      // changelog 토픽 생성 설정
+                                .withRetention(Duration.ofMinutes(7))  // 로컬 Rocks db 보관 기관
 
                 ); // ktable로 상태 저장 (stateful)
 
